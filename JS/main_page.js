b7169c2dca44be2325f82f68d5b45ab796c506d1
Timmy Groves <timmy.r.groves@gmail.com>
2024-04-05 09:47:12 -0400

    moved navbar

--- JS/main_page.js
+++ JS/main_page.js
@@ -44,10 +44,10 @@ if(userDoc.data().isAdmin){
 
 if(window.innerWidth < 500) {
   document.getElementsByClassName("dropdownnav")[0].style.display = "flex";
-  document.getElementsByClassName("navbar")[0].style.display = "none";
+  document.getElementsByClassName("navbar")[0].style.visibility = "hidden";
 } else {
   document.getElementsByClassName("navbar")[0].style.display = "flex";
-  document.getElementsByClassName("dropdownnav")[0].style.display = "hidden";
+  document.getElementsByClassName("dropdownnav")[0].style.visibility = "hidden";
 }
 
 
