784dc84151be9dae196aacf0589f446efec3fa1e
Timmy Groves <timmy.r.groves@gmail.com>
2024-04-09 19:01:13 -0400

    changed function to click off of dropdown and added show to css

--- JS/main_page.js
+++ JS/main_page.js
@@ -62,14 +62,17 @@ function showDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdown = document.getElementsByClassName("dropdown-content")[0];
-        if (dropdown.classList.contains('show')) {
-          dropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
+  }
+}
 
 
 
