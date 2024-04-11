16badfa8d0f9f6adfe98c304e9856b17545b2441
Timmy Groves <timmy.r.groves@gmail.com>
2024-04-11 11:06:05 -0400

    changed requests so that something shows up when no one is requested

--- JS/requests.js
+++ JS/requests.js
@@ -152,7 +152,8 @@ export const showItems = async function () {
       tutor_requests.appendChild(row);
     }
   });
-  if (document.getElementById("tutor_requests") == null) {
+  println(document.getElemtntById("tutor_requests").innerHTML);
+  if (document.getElementById("tutor_requests").innerHTML == null) {
     let req = document.getElementById("tutor_requests");
     let none = document.createElement("div");
     let text = document.createElement("h1");
