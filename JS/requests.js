21013e93e7fd654360563f63fe495094ba2238ef
Timmy Groves <timmy.r.groves@gmail.com>
2024-04-11 13:28:40 -0400

    changed print

--- JS/requests.js
+++ JS/requests.js
@@ -152,7 +152,7 @@ export const showItems = async function () {
       tutor_requests.appendChild(row);
     }
   });
-  println(document.getElemtntById("tutor_requests").innerHTML);
+  System.out.println(document.getElemtntById("tutor_requests").innerHTML);
   if (document.getElementById("tutor_requests").innerHTML == null) {
     let req = document.getElementById("tutor_requests");
     let none = document.createElement("div");
